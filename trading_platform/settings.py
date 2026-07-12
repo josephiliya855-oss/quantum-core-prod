7728ad8c66b7384e32055d6a674945d702bf0f6c
AlgoDeveloper <sandbox@local.dev>
2026-07-12 03:41:03 +0100

    Configure production static files collection

--- trading_platform/settings.py
+++ trading_platform/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard',
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 MIDDLEWARE = [
