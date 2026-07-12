f042b18f59faed2fb47c10174665343119368805
AlgoDeveloper <sandbox@local.dev>
2026-07-12 03:11:50 +0100

    Allow all hosts for production deployment

--- trading_platform/settings.py
+++ trading_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!ey@rg&hu@7eti)d^=0ty7mejo*b=zu*9h_k262n&taoougv5_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
